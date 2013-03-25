6c58b3845861a8629ee382c35f1e1100ea071cb6
Kutuzov Gregory <morgot.gregory@gmail.com>
2013-03-25 18:24:27 +0400

    Подключил расчет волновых функций. Добавил выборку нужных и вывод в файл. Переделал потенциал под яму, ввел нужные переменные

--- atom_bound_states/main.cpp
+++ atom_bound_states/main.cpp
@@ -4,31 +4,46 @@ using namespace std;
 
 #include "alglib/ap.h"
 #include "alglib/linalg.h"
+#include <fstream>
+#include <vector>
+#include <cmath>
 
-double potential_V( double r );
+double potential_V( double r, double a, double V_0 );
 
 void print( alglib::real_2d_array A );
 
-double F( double r, int l );
+double F( double r, int l, double a, double V_0 );
 
 int main()
 {
     // Ввод входных параметров:
     int N, l;
-    double h, r_max, E;
+    double h, V_0, a, r_max;
     cout << "Введите количество шагов N: ";
     cin >> N;
     cout << endl;
 
-    cout << "Введите r_max: ";
-    cin >> r_max;
+    cout << "Введите V_0: ";
+    cin >> V_0;
     cout << endl;
+
+    cout << "Введите a: ";
+    cin >> a;
+    cout << endl;
+
+    r_max = 2*a;
 
     cout << "Введите l: ";
     cin >> l;
     cout << endl;
 
+    // Расчет матриц A и B:
+
     alglib::real_2d_array A, B;
+    alglib::real_1d_array E;
+    alglib::real_2d_array f;
+    E.setlength( N );
+    f.setlength( N, N );
     A.setlength( N, N );
     B.setlength( N, N );
 
@@ -40,23 +55,56 @@ int main()
     for ( int i = 0; i < A.rows(); i++){
 
         if( i != 0 ) {
-            A[i][i-1] = F( r-h, l )*h*h/12 - 1;
+            A[i][i-1] = F( r-h, l, a, V_0 )*h*h/12 - 1;
             B[i][i-1] = h*h/12;
         }
-        A[i][i] = 2 + 5*F( r, l )*h*h/6;
+        A[i][i] = 2 + 5*F( r, l, a, V_0 )*h*h/6;
         B[i][i] = 10*h*h/12;
 
         if( i != A.cols()-1 ){
-            A[i][i+1] = F( r + h, l)*h*h/12 -1;
+            A[i][i+1] = F( r + h, l, a, V_0 )*h*h/12 -1;
             B[i][i+1] = h*h/12;
         }
         r += h;
     }
 
-    print(A);
-    cout << endl;
-    print(B);
 
+    // Поиск энергий и состояний:
+
+    bool status = alglib::smatrixgevd( A, N, false, B, false, 1, 1, E, f);
+
+    // Выборка по заданному критерию
+    vector< vector< double > > solve;
+
+    double eps = 10;
+    cout << "Энергии связных состояний: "<< endl;
+
+    for (int i = 0; i < E.length(); i++){
+
+        if ( E[i] > 0 ) break;
+
+        int doub_a = int(1.5*a/h);
+
+        if ( abs( f[ int( doub_a ) ][i] - exp( -sqrt( abs( E[i] ) ) * a ) ) < eps ){
+            vector< double > temp( f.cols(), 0);
+            cout << "E"<< i <<" = " << E[i] << endl;
+            for ( int j = 0; j < f.cols(); j++ ){
+                temp[j] = f[j][i];
+            }
+            solve.push_back(temp);
+        }
+    }
+    // Вывод в файл
+    ofstream output("output.txt");
+
+    for(int i=0; i < solve[0].size(); i++){
+        for(int j = 0; j < solve.size(); j++){
+            output << h*(i+1) << " "<< solve[j][i] << " ";
+        };
+    output << std::endl;
+    };
+
+    output.close();
 
 
     return 0;
@@ -64,17 +112,20 @@ int main()
 
 // Определение потенциала V(r):
 
-double potential_V ( double r ){
+double potential_V ( double r, double a, double V_0 ){
 
-    double k = 2.0;
-    return k/r;
+    if( r > a ) return 0;
+
+    else {
+        return -V_0;
+    }
 }
 
-// Определение потенциала F(r, l):
+// Определение пКотенциала F(r, l):
 
-double F( double r, int l ){
+double F( double r, int l, double a, double V_0 ){
 
-    return potential_V(r) + ( double(l)*( double( l + 1 ) ) )/r/r;
+    return potential_V(r, a, V_0) + ( double(l)*( double( l + 1 ) ) )/r/r;
 }
 
 void print( alglib::real_2d_array A ){
